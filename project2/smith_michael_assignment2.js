8ce8e49089d7411dd2fc2104cdd41f9189cb7e0d
Michael Smith Jr <mikesmithjr@fullsail.edu>
2012-04-02 23:05:20 -0500

    Imported code from last week to assist with assignment

--- project2/smith_michael_assignment2.js
+++ project2/smith_michael_assignment2.js
@@ -1,1 +1,55 @@
+// Assignment: Project 2 SDI Online
+// Author: Michael Smith
+// Story: Flying an Radio-Controlled Airplane
+
+
+
+// Variable Assignments
+
+var	weatherCondition = "Good";
+	planeStatus = "Loaded"
+	maxFliteTime = 15;
+	flightTime = 10;
+	safeLanding = true;
+
+// How's the weather?
+
+if (weatherCondition = "Good") {
+	console.log('Let\'s go flying!');
+} else {
+	console.log('There won\'t be any flying today');
+};
+
+// Is the plane loaded in the truck?
+
+if (planeStatus = "Loaded") {
+	console.log("Off to the flying field.");
+} else {
+	console.log("Better get the epuipment loaded.");
+};
+
+// How long have you been in the air?
+
+if (flightTime >= 15) {
+	console.log("You are out of battery land quick before you crash!!!");
+} else {
+	console.log("You have a few more minutes of flying.");
+};
+
+// Did you land the plane safely?
+
+if (safeLanding === true) {
+	console.log("Let's fly again!!!");
+} else {
+	console.log("Back home for repairs.")
+};
+
+
+
+
+
+
+
+
+
 alert("JavaScript works!");
