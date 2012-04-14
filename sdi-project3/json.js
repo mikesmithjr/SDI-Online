d1842565bad9453d3f259604829d88c64dc32283
Michael Smith Jr <mikesmithjr@fullsail.edu>
2012-04-13 22:48:54 -0500

    Added JSON data

---
+++ sdi-project3/json.js
@@ -0,0 +1,27 @@
+//JSON data file for assignment 3
+//Author: Michael Smith
+
+
+var json = {
+	"pilots": [
+		{
+			"name": "Joaquin",
+			"flyingExperience": "intermediate",
+			"flightTime": 10
+
+		},
+		{
+			"name": "Al",
+			"flyingExperience": "beginner",
+			"flightTime": 5
+
+		},
+		{
+			"name": "Gary",
+			"flyingExperience": "advanced",
+			"flightTime": 15
+
+		}
+	]
+
+};
