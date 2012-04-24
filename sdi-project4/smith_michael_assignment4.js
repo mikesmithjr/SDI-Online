f6d798d08e6d4cee5e296c188d7bd6fcdf845105
Michael Smith Jr <mikesmithjr@fullsail.edu>
2012-04-24 00:26:13 -0500

    Started work on email validation function.

--- sdi-project4/smith_michael_assignment4.js
+++ sdi-project4/smith_michael_assignment4.js
@@ -1,1 +1,65 @@
+// Assignment: Project 4 SDI Online
+// Author: Michael Smith
+// Building Library
+
+
+
+
+var mikesLib = function () {
+
+	checkEmail = function(emailAddress) {
+		var email = emailAddress
+		var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$/
+		if (emailPattern.test(email)) {
+			console.log(email + " is a valid email address.")
+		} else {
+			console.log(email + " is not a valid email address.")
+		};
+		return {
+			"checkEmail" : checkEmail
+		};
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+};
+
+var emailValidation = mikesLib.checkEmail();
+
+console.log(emailValidation("snuffyms@gmail.com"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 alert("JavaScript works!");
